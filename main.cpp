fec93fb86a158b2502d65ab96ccf507ea0a30507
Kepei Lei <klei@wpi.edu>
2019-11-19 12:40:19 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "pcap.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    pcap_t *descr;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    if (argc != 2) {
+        cout << "Illegal input, must put only the file path" << endl;
+    } else {
+        descr = pcap_open_offline(argv[1], errbuf);
+    }
+    if (descr == NULL) {
+        cout << "pcap_open() failed: " << errbuf << endl;
+        return 1;
+    }
+
+    if (pcap_datalink(descr) != DLT_EN10MB) {
+        cout << "A package not captured from Ethernet, aborting" << endl;
+    }
+
+
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}
