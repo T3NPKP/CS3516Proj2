143d96d1421ef197085af30ce2f7caf15b30194f
Kepei Lei <klei@wpi.edu>
2019-11-19 13:28:00 -0500

    other test files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <pcap.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
 using namespace std;
+
+void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet);
 
 int main(int argc, char* argv[]) {
     pcap_t *descr;
@@ -24,8 +30,20 @@ int main(int argc, char* argv[]) {
 
     cout << "File opened, analyzing..." << endl;
 
-
+    if (pcap_loop(descr, 0, packetHandler, NULL) < 0) {
+        cout << "pcap_loop() failed: " << pcap_geterr(descr);
+        return 1;
+    }
 
     cout << "Hello, World!" << endl;
     return 0;
+}
+
+void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
+    const struct ether_header *ethernetHeader;
+    const struct ip *ipHeader;
+    const struct udphdr *udpHeader;
+    char sourceIp[INET_ADDRSTRLEN];
+    char destIp[INET_ADDRSTRLEN];
+
 }
