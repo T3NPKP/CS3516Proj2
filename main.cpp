ed799484f82789b15b6a2eabe9aa8cdf62c1516d
Kepei Lei <klei@wpi.edu>
2019-11-19 22:59:19 -0500

    debugging

--- main.cpp
+++ main.cpp
@@ -163,7 +163,7 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
     // IP address
     ipHeader = (struct ip*)(packet + sizeof(struct ether_header));
     char* destIPStr = inet_ntoa(ipHeader->ip_dst);
-    char* sourceIPStr = inet_ntoa(ipHeader->ip_dst);
+    char* sourceIPStr = inet_ntoa(ipHeader->ip_src);
     cout << sourceIPStr << endl;
     cout << destIPStr << endl;
     cout << endl;
